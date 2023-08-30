8d00c7a4f3e52e5bd2185365c8135bc06c8a945f
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-08-30 14:31:18 -0400

    Work on setting up move graph

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style.css";
 function component() {
   const element = document.createElement("div");
   element.innerText = "Knights Travails Project";
@@ -9,3 +10,24 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+let arrayX = [0, 1, 2, 3, 4, 5, 6, 7];
+let arrayY = [0, 1, 2, 3, 4, 5, 6, 7];
+let knightPos = [0, 0];
+//knight can make up to 8 possible moves:
+//[+2,+1], [+1,+2], [+2,-1], [+1,-2], [-1,-2], [-2,-1], [-2,+1], [-1,+2]
+let possibleMoves = [
+  [2, 1],
+  [1, 2],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
+];
+function fullEdgeList() {
+  let fullEdgeList = [];
+}
+
+function currentEdgeList() {}
